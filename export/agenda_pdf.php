04a486637d95018445fdaf3f7d67e44423407942
Jacob <jacowest@gmail.com>
2026-01-20 21:07:36 +1100

    250

--- export/agenda_pdf.php
+++ export/agenda_pdf.php
@@ -129,7 +129,7 @@ $css = (file_exists($cssPath) ? file_get_contents($cssPath) : '');
 // Add logo if configured and exists
 $logoHtml = '';
 if (defined('LOGO_PATH') && LOGO_PATH && file_exists(LOGO_PATH)) {
-    $logoWidth = defined('LOGO_WIDTH') ? LOGO_WIDTH : 10;
+    $logoWidth = defined('LOGO_WIDTH') ? LOGO_WIDTH : 250;
     $logoHtml = '<div style="text-align:center; margin-bottom:15px;"><img src="' . LOGO_PATH . '" style="max-width:' . $logoWidth . 'px; height:auto;" alt="Logo"></div>';
 }
 
